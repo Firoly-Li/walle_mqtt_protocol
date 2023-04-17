4369a41470448dfbb1714c9296d493a4151766be
Firoly_Li <lhyfiroly@163.com>
2023-04-17 20:58:38 +0800

    1、添加测试方法，添加README信息

--- src/lib.rs
+++ src/lib.rs
@@ -134,3 +134,31 @@ impl Encoder for Topic {
         Ok(topic_len + 3)
     }
 }
+
+
+
+#[cfg(test)]
+mod tests {
+    use bytes::Bytes;
+
+    use crate::v4::builder::MqttMessageBuilder;
+
+
+    #[test]
+    fn test() {
+        let connect = MqttMessageBuilder::connect()
+        .client_id("client_01")
+        .keep_alive(10)
+        .clean_session(true)
+        .username("rump")
+        .password("mq")
+        .protocol_level(crate::MqttVersion::V4)
+        .retain(false)
+        .will_qos(crate::QoS::AtLeastOnce)
+        .will_topic("/a")
+        .will_message(Bytes::from_static(b"offline"))
+        .build();
+
+        println!("connect = {:?}", connect);
+    }
+}
